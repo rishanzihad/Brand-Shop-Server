59683cfcf6094710fd2dbb494328a288a0d6b56d
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-10-19 11:33:48 -0700

    delete Method Added

--- index.js
+++ index.js
@@ -60,6 +60,22 @@ async function run() {
     console.log(result)
     res.send(result)
   })
+  app.get('/carts/:id',async(req,res)=>{
+    const id =req.params.id
+    const query ={_id: new ObjectId(id)}
+    const result =await addCartCollection.findOne(query)
+    console.log(result)
+    res.send(result)
+  })
+ 
+  app.delete("/carts/:id", async(req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const query = { _id: new ObjectId(id) };
+    const result = await addCartCollection.deleteOne(query);
+    console.log(result)
+    res.send(result);
+  });
   app.put('/products/:id',async(req,res)=>{
     const id =req.params.id
     const data =req.body
@@ -77,6 +93,7 @@ async function run() {
         rating:data.rating
       }
     }
+    
 
     const result =await productCollection.updateOne(filter,updatedData,options)
     res.send(result)
