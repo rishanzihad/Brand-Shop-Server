bb9c54b665f7ac9bbe74c99d959862600abe0699
rishanzihad <138320123+rishanzihad@users.noreply.github.com>
2023-10-20 01:46:38 -0700

    removing node_modules

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express =require("express")
 const cors =require('cors')
 const app =express()
-
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
-//techServer
-//l34MFlaIoyJKjIOi
+
 const port =process.env.PORT || 4005
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = "mongodb+srv://techServer:l34MFlaIoyJKjIOi@cluster0.qjppvab.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qjppvab.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
